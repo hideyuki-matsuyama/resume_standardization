24ac10cfe229ba5ccf6700745a90e155b73e1660
hideyuki matsuyama <choripon@gmail.com>
2019-09-21 21:06:56 +0900

    👍ログアウトの遷移先をログイン画面にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+# 基底コントローラー
 class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :administrator
+      rails_admin_path
+    else
+      root_path
+    end
+  end
 end
